bb88e0239b89cb9f3633b36de59b6b1958c14972
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-18 11:45:12 +0530

    Create 3_Tranverse_Arrays.py

---
+++ 3_Tranverse_Arrays.py
@@ -0,0 +1,12 @@
+#### Lets practice Arrays
+arr1 = array(i,[1,2,3,4,5])
+print(arr1)
+
+arr1.insert(5,6)
+print(arr1)
+
+### Traverse Array
+def traverseArray(array):
+  for i in array:
+    print(i)
+traverseArray(arr1)
