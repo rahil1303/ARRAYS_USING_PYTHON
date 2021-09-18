5a794289778e753de0002820f6ed5116d4f05732
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-18 11:24:17 +0530

    Create 2_Arrays_Insertion_Operation.py

---
+++ 2_Arrays_Insertion_Operation.py
@@ -0,0 +1,8 @@
+#using the array created above arr1
+#Here we insert an element at the end of the array
+arr1.insert(7,9)
+print(arr1)
+
+#Here we insert an element in middle of the array
+arr1.insert(2,8)
+print(arr1)
