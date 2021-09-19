b07a9ec8c5cfe5e7ef2f6a9b4ee05d3e70d90812
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-19 16:30:52 +0530

    Create 8_Insertion_of_elements_in_2D_Array.py

---
+++ 8_Insertion_of_elements_in_2D_Array.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+newarr = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+print(newarr)
+
+new2Darr = np.insert(newarr,0,[[17,18,19,20]],axis = 0) ### Array
+print(new2Darr)
+
+new2Darr = np.insert(newarr,0,[[21,22,23,24]],axis = 1) ### Column
+print(new2Darr)
