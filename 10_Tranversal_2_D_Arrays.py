290cbde188416fc58892f78d1082e58c3794fd7d
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-19 17:01:36 +0530

    Create 10_Tranversal_2_D_Arrays.py

---
+++ 10_Tranversal_2_D_Arrays.py
@@ -0,0 +1,11 @@
+from array import numpy as np
+import *
+
+new2Darr = np.array([1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]]
+print(new2Darr)
+def traverse2Darray(array):
+                    for i in range(len(array)):
+                      for j in range(len(array[0])):
+                        print(array[i][j]
+traverse2Darray(new2Darr)
+                 
