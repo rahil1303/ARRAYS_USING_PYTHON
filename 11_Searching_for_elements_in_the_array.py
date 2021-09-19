0e6b8379749f17a79cbae20845c38caf782a34e8
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-19 17:29:25 +0530

    Create 11_Searching_for_elements_in_the_array.py

---
+++ 11_Searching_for_elements_in_the_array.py
@@ -0,0 +1,12 @@
+newarr = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+print(newarr)
+
+def searchelements(array,value):
+  for i in range(len(array)):
+    for j in range(len(array[0]):
+                   if array[i][j] == value:
+                    return "The number was found at the index "+str(i)+" "+str(j)
+  return "The element is not found"
+searchelements(newarr,13)
+                 
+           
